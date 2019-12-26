5bbfeda035da84becb350e1c1942d42e655b455b
Olga Matveeva <olga.matveeva.0@gmail.com>
2019-12-25 23:48:24 -0800

    Returned to algorithm from the book

--- union-find/src/main/java/Main.java
+++ union-find/src/main/java/Main.java
@@ -1,26 +1,23 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 
 /**
  * Created by olga on 21.12.2019.
  */
 public class Main {
     public static void main(String[] args) {
-
-        int n = StdIn.readInt();
+        int[] input = StdIn.readAllInts();
+        int n = input.length;
+        System.out.println(n);
         UnionFind uf = new UnionFind(n);
-        while (!StdIn.isEmpty()) {
-            int p = StdIn.readInt();
-            int q = StdIn.readInt();
+        for (int i = 0; i < input.length - 1; i++) {
+            int p = input[i];
+            int q = input[i+1];
             if (uf.connected(p, q)) continue;
             uf.union(p, q);
             StdOut.println(p + " " + q);
         }
-        StdOut.println(uf.count() + "components");
+        StdOut.println(uf.count() + " components");
     }
 }

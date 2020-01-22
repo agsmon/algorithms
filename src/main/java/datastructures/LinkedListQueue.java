2b013faddd7b02911e3d4864038868faa5b8e73c
Olga Matveeva <olga.matveeva.0@gmail.com>
2020-01-21 17:52:01 -0800

    data structures: linked list queue

--- src/main/java/datastructures/LinkedListQueue.java
+++ src/main/java/datastructures/LinkedListQueue.java
@@ -1,4 +1,42 @@
 package datastructures;
 
-public class LinkedListQueue {
+public class LinkedListQueue<Item> {
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    private Node first;
+    private Node last;
+    private int itemsAmount;
+
+    public boolean isEmpty() {
+        return itemsAmount == 0;
+    }
+
+    public void add(Item item) {
+        // add new last item to the queue
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldLast.next = last;
+        }
+        itemsAmount++;
+    }
+
+    public Item get() {
+        // remove first item
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()) {
+            last = null;
+        }
+        itemsAmount--;
+        return item;
+    }
+
 }
--- src/main/java/datastructures/Main.java
+++ src/main/java/datastructures/Main.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //ResizingArrayStack<Integer> stack = new ResizingArrayStack<Integer>();
-        LinkedListStack<Integer> stack = new LinkedListStack<Integer>();
+        //tryStack();
+        tryQueue();
+    }
+
+    private static void tryStack() {
+        //ResizingArrayStack<Integer> stack = new ResizingArrayStack<>();
+        LinkedListStack<Integer> stack = new LinkedListStack<>();
         System.out.println("To put an item into stack enter it. To pop an item enter \"pop\". To stop enter \"stop\". Your input: ");
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNext()) {
@@ -26,7 +31,26 @@ public class Main {
             }
             System.out.println("Your input: ");
         }
-
     }
 
+    private static void tryQueue() {
+        LinkedListQueue<String> queue = new LinkedListQueue<>();
+        System.out.println("To add an item into queue enter it. To get an item enter \"get\". To stop enter \"stop\". Your input: ");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String next = scanner.next();
+            if (next.contentEquals("stop")) {
+                break;
+            } else if (next.contentEquals("get")) {
+                if (queue.isEmpty()) {
+                    System.out.println("Queue is empty");
+                } else {
+                    System.out.println("The item is: " + queue.get());
+                }
+            } else {
+                queue.add(next);
+            }
+            System.out.println("Your input: ");
+        }
+    }
 }
